00aa98e31840ccb568896910dfb3541c7cdaf326
Devanshu Ukey <142024660+dukey20@users.noreply.github.com>
2023-12-10 23:08:47 +0530

    Add files via upload

---
+++ Portfolio Website_Devanshu_Ukey/README.md
@@ -0,0 +1,2 @@
+Hello I'm Devanshu Ukey
+My Portfolio
---
+++ Portfolio Website_Devanshu_Ukey/index.js
@@ -0,0 +1,40 @@
+
+let TechnologiesUsed = tns({
+    container: ".my-slider",
+    "slideBy": "2",
+    "speed": 1000,
+    "nav": false,
+    autoplay: true,
+    controls: false,
+    autoplayButtonOutput: false,
+    responsive: {
+        1600: {
+            items: 7,
+            gutter: 20
+        },
+        1024: {
+            items: 6,
+            gutter: 20
+        },
+        768: {
+            items: 3,
+            gutter: 20
+        },
+        480: {
+            items: 1,
+            gutter: 20
+        },
+    }
+})
+
+function SmartLed()
+{
+    window.open(
+        'https://youtu.be/phHhytCdNdI',
+        '_blank'
+      );
+} 
+
+
+
+
